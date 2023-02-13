d6ff89871dd5460757aa71b074de55e885769cfb
d9ndthang <d9ndthang@cmcglobal.vn>
2023-02-13 09:42:10 +0700

    Update Djoser"

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -159,3 +159,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #         'NAME': 'reservations',
 #     }
 # }
+SIMPLE_JWT = {
+    'AUTH_HEADER_TYPES':('JWT',),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=3)
+}
